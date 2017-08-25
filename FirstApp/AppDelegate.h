678a52e2ff5395d9f6368e1bd71722a43163d1a7
sparky192 <mphadate@smu.edu>
2017-08-24 19:29:32 -0500

    Initial Commit

---
+++ FirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstApp
+//
+//  Created by Mandar Phadate on 8/24/17.
+//  Copyright © 2017 Mandar Phadate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstApp
+//
+//  Created by Mandar Phadate on 8/24/17.
+//  Copyright © 2017 Mandar Phadate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
